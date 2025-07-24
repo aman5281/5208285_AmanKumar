544323a24a432240f07f10724f609bb1c46264fa
Aman Kumar <amankumar52819@gmail.com>
2025-07-24 21:11:49 +0530

    Included Double

--- Git/index.java
+++ Git/index.java
@@ -4,5 +4,8 @@ public class index {
     public static void main(String[] args) {
         // Print message to console
         System.out.println("Hello, World!");
+
+//        This code is from Dev branch
+        double d = 0.0;
     }
 }
